35840c80c8d846ad2b39444b124a39f55448a8a3
amritraj <amritraj1510@gmail.com>
2017-08-20 17:46:52 +0530

    index changed

--- mail2/mail.php
+++ mail2/mail.php
@@ -20,8 +20,8 @@ if (isset($_POST['name'])&&isset($_POST['email'])&&isset($_POST['message']))
 		$mail->isSMTP();                                  
 		$mail->Host = 'smtp.gmail.com';
 		$mail->SMTPAuth = true;              
-		$mail->Username = '';
-		$mail->Password = '';
+		$mail->Username = 'rajamrit1510@gmail.com';
+		$mail->Password = 'ASDF1234';
 		$mail->SMTPSecure = 'tls';                 
 		$mail->Port = 587;          
 
